82ecd1f03903c0d4d0d723bd275c2684331bb579
Dimitar Mitev <d.mitev920123@gmail.com>
2025-01-22 16:48:23 +0200

    feat: setup liveblocks

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { useParams } from "next/navigation";
+
+export function Room({ children }: { children: ReactNode }) {
+  const params = useParams();
+  return (
+    <LiveblocksProvider
+      publicApiKey={
+        "pk_dev_FI8gFqQhmkVFUTv4YCRq5hZk2FlAQqyK4t8aomTBdDzQ3fAKsWMvtBpeAMAKWqAm"
+      }
+    >
+      <RoomProvider id={params.documentId as string}>
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
