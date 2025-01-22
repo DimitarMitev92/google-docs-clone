9dc697da023e12935656f2abffd6f273a0050e46
Dimitar Mitev <d.mitev920123@gmail.com>
2025-01-22 18:05:07 +0200

    feat: liveblocks authentication

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -11,11 +11,7 @@ import { useParams } from "next/navigation";
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
   return (
-    <LiveblocksProvider
-      publicApiKey={
-        "pk_dev_FI8gFqQhmkVFUTv4YCRq5hZk2FlAQqyK4t8aomTBdDzQ3fAKsWMvtBpeAMAKWqAm"
-      }
-    >
+    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
       <RoomProvider id={params.documentId as string}>
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
